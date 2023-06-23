f079e142390dbac7087c5e3e2775ed583eddef19
Ludi <ludi.adressemail@gmail.com>
2023-06-23 14:34:59 +0200

    exo4 done

--- exo4.php
+++ exo4.php
@@ -36,9 +36,39 @@ $arrayB = [85, "toi", 95, "la", 65, 94, 85, "avec", 37, "chat"];
         <!-- QUESTION 1 -->
         <section class="exercice">
             <h2 class="exercice-ttl">Question 1</h2>
-            <p class="exercice-txt">Déclarer une fonction qui prend en paramètre un tableau et retourne la chaîne de caractère HTML permettant d'afficher les valeurs du tableau sous la forme d'une liste.</p>
+            <p class="exercice-txt">Déclarer une fonction qui prend en paramètre un tableau
+                et retourne la chaîne de caractère HTML permettant d'afficher les valeurs
+                du tableau sous la forme d'une liste.</p>
             <div class="exercice-sandbox">
 
+                <?php
+
+                /**
+                 * Return every value from array in HTML List
+                 * 
+                 * @param array $array - input array
+                 * @return string - HTML list
+                 */
+
+                // function getListFromArray(array $array): string
+                // {
+                //     $htmlList = '';
+                //     foreach ($array as $item) {
+                //         $htmlList .= "<li>$item</li>";
+                //     }
+                //     return "<ul>$htmlList</ul>";
+                // }
+                // echo getListFromArray($array);
+
+                function getListFromArray(array $array): string
+                {
+                    $a = array_map(fn ($value) => "<li>$value</li>", $array);
+                    return '<ul>' . implode('', $a) . '</ul>';
+                }
+                echo getListFromArray($array);
+
+
+                ?>
             </div>
         </section>
 
@@ -47,6 +77,34 @@ $arrayB = [85, "toi", 95, "la", 65, 94, 85, "avec", 37, "chat"];
             <h2 class="exercice-ttl">Question 2</h2>
             <p class="exercice-txt">Déclarer une fonction qui prend en paramètre un tableau d'entiers et retourne uniquement les valeurs paires. Afficher les valeurs du tableau sous la forme d'une liste HTML.</p>
             <div class="exercice-sandbox">
+
+                <?php
+
+                // function getEvenFromArray(array $array): array
+                // {
+                //     $arrayEven = [];
+                //     foreach ($array as $value) {
+                //         if ($value % 2 == 0) {
+                //             $arrayEven[] = $value;
+                //         }
+                //     }
+                //     return $arrayEven;
+                // }
+                // $evenValues = getEvenFromArray($array);
+                // echo getListFromArray($evenValues);
+
+
+                function getEvenFromArray(array $array): array
+                {
+                    $b = array_filter($array, function ($valu) {
+                        return $valu % 2 === 0;
+                    });
+                    return $b;
+                }
+                $evenValues2 = getEvenFromArray($array);
+                var_dump(getEvenFromArray($evenValues2));
+
+                ?>
 
             </div>
         </section>
@@ -57,7 +115,34 @@ $arrayB = [85, "toi", 95, "la", 65, 94, 85, "avec", 37, "chat"];
             <p class="exercice-txt">Déclarer une fonction qui prend en paramètre un tableau d'entiers et retourne uniquement les entiers d'index pair</p>
             <div class="exercice-sandbox">
 
+                <?php
+                // function getEvenIndexFromArray(array $array): array
+                // {
+                //     $arrayIndex = [];
+                //     foreach ($array as $index => $value) {
+                //         if ($index % 2 == 0) {
+                //             $arrayIndex[] = $value;
+                //         }
+                //     }
+                //     return $arrayIndex;
+                // }
+                // $evenIndex = getEvenIndexFromArray($array);
+                // echo getListFromArray($evenIndex);
+
+                function getEvenIndexFromArray(array $array): array
+                {
+                    $evenIndexes = array_filter($array, function ($val) {
+                        return $val % 2 === 0;
+                    }, ARRAY_FILTER_USE_KEY);
+                    return $evenIndexes;
+                }
+                $evenIdx = getEvenIndexFromArray($array);
+                echo getListFromArray($evenIdx);
+
+
+                ?>
             </div>
+
         </section>
 
         <!-- QUESTION 4 -->
@@ -66,15 +151,45 @@ $arrayB = [85, "toi", 95, "la", 65, 94, 85, "avec", 37, "chat"];
             <p class="exercice-txt">Déclarer une fonction qui prend en paramètre un tableau d'entiers. La fonction doit retourner les valeurs du tableau mulipliées par 2.</p>
             <div class="exercice-sandbox">
 
+                <?php
+                function getMultiplyBy2(array $array): array {
+                    $arrayDouble = [];
+                    foreach ($array as $value) {
+                        $arrayDouble[] = $value * 2;
+                    }
+                    return $arrayDouble;
+                }
+                
+                var_dump(getMultiplyBy2($array));
+
+
+                // $double = array_map(fn($value) => $value * 2, $array);
+                // var_dump($double);
+                ?>
             </div>
+
+
         </section>
 
         <!-- QUESTION 4 bis -->
         <section class="exercice">
             <h2 class="exercice-ttl">Question 4 bis</h2>
-            <p class="exercice-txt">Déclarer une fonction qui prend en paramètre un tableau d'entiers et un entier. La fonction doit retourner les valeurs du tableau divisées par le second paramètre</p>
+            <p class="exercice-txt">Déclarer une fonction qui prend en paramètre un tableau 
+                d'entiers et un entier. La fonction doit retourner les valeurs du tableau divisées par le second paramètre</p>
             <div class="exercice-sandbox">
 
+            <?php
+            function divideArray(array $array, int $number): array {
+                $newArray = [];
+                foreach ($array as $values2) {
+                    $newArray[] = $values2 / $number;
+                }
+                return $newArray;
+            }
+
+            var_dump(divideArray($array, 2));
+            
+            ?>
             </div>
         </section>
 
@@ -84,6 +199,14 @@ $arrayB = [85, "toi", 95, "la", 65, 94, 85, "avec", 37, "chat"];
             <p class="exercice-txt">Déclarer une fonction qui prend en paramètre un tableau d'entiers ou de chaînes de caractères et retourne le tableau sans doublons</p>
             <div class="exercice-sandbox">
 
+                <?php
+                function getUnique(array $arrayA): array {
+                    return array_unique($arrayA);
+                }
+
+                var_dump(getUnique($arrayA));
+
+                ?>
             </div>
         </section>
 
@@ -93,14 +216,33 @@ $arrayB = [85, "toi", 95, "la", 65, 94, 85, "avec", 37, "chat"];
             <p class="exercice-txt">Déclarer une fonction qui prend en paramètre 2 tableaux et retourne un tableau représentant l'intersection des 2</p>
             <div class="exercice-sandbox">
 
-            </div>
+            <?php
+            function getIntersect(array $arrayA, array $arrayB): array {
+                    return array_intersect($arrayA, $arrayB);
+                }
+
+                var_dump(getIntersect($arrayA, $arrayB));
+            ?>
+
+            </div>            
         </section>
 
         <!-- QUESTION 7 -->
         <section class="exercice">
             <h2 class="exercice-ttl">Question 7</h2>
-            <p class="exercice-txt">Déclarer une fonction qui prend en paramètre 2 tableaux et retourne un tableau des valeurs du premier tableau qui ne sont pas dans le second</p>
+            <p class="exercice-txt">Déclarer une fonction qui prend en paramètre 2 tableaux et 
+                retourne un tableau des valeurs du premier tableau qui ne sont pas dans le second</p>
             <div class="exercice-sandbox">
+
+            <?php
+            function getDiffrences(array $arrayA, array $arrayB): array {
+                $arrayDiff = array_diff($arrayA, $arrayB);
+                return $arrayDiff;
+            }
+
+            $arrayVersus = getDiffrences($arrayA, $arrayB);
+            var_dump($arrayVersus);
+            ?>
 
             </div>
         </section>
@@ -109,8 +251,24 @@ $arrayB = [85, "toi", 95, "la", 65, 94, 85, "avec", 37, "chat"];
         <!-- QUESTION 8 -->
         <section class="exercice">
             <h2 class="exercice-ttl">Question 8</h2>
-            <p class="exercice-txt">Réécrire la fonction précédente pour lui ajouter un paramètre booléen facultatif. Si celui-ci est à true, le tableau retourné sera sans doublons</p>
+            <p class="exercice-txt">Réécrire la fonction précédente pour lui ajouter un paramètre 
+                booléen facultatif. Si celui-ci est à true, le tableau retourné sera sans doublons</p>
             <div class="exercice-sandbox">
+            <?php
+
+            function getDifferences(array $arrayA, array $arrayB, bool $isset = true): array {
+                if($isset === true) {
+                    $arrayDiff = array_diff($arrayA, $arrayB);
+                    return $arrayDiff;
+                }
+                else {
+                    return $arrayA;
+                }
+            }
+
+            $isset = true;
+            var_dump(getDifferences($arrayA, $arrayB, $isset));
+            ?>
 
             </div>
         </section>
@@ -119,8 +277,20 @@ $arrayB = [85, "toi", 95, "la", 65, 94, 85, "avec", 37, "chat"];
         <!-- QUESTION 9 -->
         <section class="exercice">
             <h2 class="exercice-ttl">Question 9</h2>
-            <p class="exercice-txt">Déclarer une fonction qui prend en paramètre un tableau et un entier et retourne les n premiers éléments du tableau.</p>
+            <p class="exercice-txt">Déclarer une fonction qui prend en paramètre un tableau et 
+                un entier et retourne les n premiers éléments du tableau.</p>
             <div class="exercice-sandbox">
+                
+            <?php
+
+                function getFirstElements(array $array, int $offset, int $length): array {
+                    return array_slice($array, $offset, $length);
+                     
+                }
+
+                var_dump(getFirstElements($array, 0, 1));
+
+            ?>
 
             </div>
         </section>
